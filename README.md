934de513cb0358f08e550da7a9f4113c46060ff8
ANiket8210 <aniketverma8210@gmail.com>
2026-02-14 20:44:23 +0530

    added README and handled CORS error

---
+++ README.md
@@ -0,0 +1,6 @@
+### Connecting to DB
+- ***update application properties with your DB data*** i.e,:
+url,username,password,dialect
+
+- in server resources, you will find **insert-data.txt**
+use it to add dummy data initially
--- server/src/main/java/org/example/server/ProductsController.java
+++ server/src/main/java/org/example/server/ProductsController.java
@@ -4,6 +4,7 @@ package org.example.server;
 import org.example.server.model.Product;
 import org.example.server.service.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +13,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api")
+@CrossOrigin
 public class ProductsController {
 
     @Autowired
